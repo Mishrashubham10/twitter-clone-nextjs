7dc8c64a7a030b627f2e4e9430920d71bbf36729
Shubham Mishra <shubhammishra10101998@gmail.com>
2025-02-03 19:29:45 +0545

    sidebar added to the codebase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-8">
+        <main className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <section className="px-2 xsm:px-4 xxl:px-8">
             <LeftBar />
-          </div>
-          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-gray-100 ">{children}</div>
-          <div className="hidden lg:flex ml-4 md:ml-8  flex-1 bg-gray-700">
+          </section>
+          <section className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">
+            {children}
+          </section>
+          <section className="hidden lg:flex ml-4 md:ml-8  flex-1 bg-gray-700">
             <RightBar />
-          </div>
-        </div>
+          </section>
+        </main>
       </body>
     </html>
   );
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,7 +1,124 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+
+const menuList = [
+  {
+    id: 1,
+    name: 'Homepage',
+    link: '/',
+    icon: 'home.svg',
+  },
+  {
+    id: 2,
+    name: 'Explore',
+    link: '/',
+    icon: 'explore.svg',
+  },
+  {
+    id: 3,
+    name: 'Notification',
+    link: '/',
+    icon: 'notification.svg',
+  },
+  {
+    id: 4,
+    name: 'Messages',
+    link: '/',
+    icon: 'message.svg',
+  },
+  {
+    id: 5,
+    name: 'Bookmarks',
+    link: '/',
+    icon: 'bookmark.svg',
+  },
+  {
+    id: 6,
+    name: 'Jobs',
+    link: '/',
+    icon: 'job.svg',
+  },
+  {
+    id: 7,
+    name: 'Communities',
+    link: '/',
+    icon: 'community.svg',
+  },
+  {
+    id: 8,
+    name: 'Premium',
+    link: '/',
+    icon: 'logo.svg',
+  },
+  {
+    id: 9,
+    name: 'Profile',
+    link: '/',
+    icon: 'profile.svg',
+  },
+  {
+    id: 10,
+    name: 'More',
+    link: '/',
+    icon: 'more.svg',
+  },
+];
 
 export default function LeftBar() {
   return (
-    <div>LeftBar</div>
-  )
+    <div className="h-screen sticky top-0 flex flex-col justify-between pt-2 pb-8">
+      {/* LOGO MENU BUTTON */}
+      <section className="flex flex-col gap-4 text-lg items-center xxl:items-start">
+        {/* LOGO */}
+        <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
+          <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
+        </Link>
+        {/* MENU LIST */}
+        <div className="flex flex-col gap-2">
+          {menuList.map((item) => (
+            <Link
+              href={`item.link`}
+              className="p-2 rounded-full hover:bg-[#181818] flex items-center gap-4"
+              key={item.id}
+            >
+              <Image
+                src={`icons/${item.icon}`}
+                alt={item.name}
+                width={24}
+                height={24}
+              />
+              <span className="hidden xxl:inline">{item.name}</span>
+            </Link>
+          ))}
+        </div>
+        {/* BUTTONS */}
+        <Link
+          href="/"
+          className="bg-white text-black rounded-full font-bold w-12 h-12 flex items-center justify-center xxl:hidden"
+        >
+          <Image src="icons/post.svg" alt="new post" width={24} height={24} />
+        </Link>
+        <Link
+          href="/"
+          className="hidden xxl:block bg-white text-black rounded-full font-bold py-2 px-20"
+        >
+          Post
+        </Link>
+      </section>
+      {/* USER */}
+      <section className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <div className="w-10 h-10 relative rounded-full overflow-hidden">
+            <Image src="/general/avatar.png" alt='Shubham Mishra' fill />
+          </div>
+          <div className="hidden xxl:flex flex-col">
+            <span className='font-bold'>Shubham Mishra</span>
+            <span className='text-sm text-textGray'>@shubhWebDev</span>
+          </div>
+        </div>
+        <div className="hidden xxl:block cursor-pointer font-bold">...</div>
+      </section>
+    </div>
+  );
 }
